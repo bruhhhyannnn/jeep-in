cdcd6240d82b1a2a25c2aef85fd89fa3086045d3
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-11-05 23:50:46 +0800

    feat(shared folder): Create a barrel file for shared folder

---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+export { default as SafeAreaContainer } from "@/components/shared/SafeAreaContainer";
+export { default as ThemedView } from "@/components/shared/ThemedView";
+export { default as ThemedText } from "@/components/shared/ThemedText";
+export { default as ButtonText } from "@/components/shared/ButtonText";
+export { default as ButtonBack } from "@/components/shared/ButtonBack";
+export { default as Icon } from "@/components/shared/Icon";
