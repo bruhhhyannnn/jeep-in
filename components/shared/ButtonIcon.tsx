a010c61bc6175f0e88f298cd87e545eabbbc2a65
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-11-06 17:13:23 +0800

    feat(buttonIcon): Create reusable button icon

---
+++ components/shared/ButtonIcon.tsx
@@ -0,0 +1,29 @@
+import { TouchableOpacity } from "react-native";
+import Icon from "@/components/shared/Icon";
+
+type ButtonTextProps = {
+  iconName: string;
+  onPress?: () => void;
+  disabled?: boolean;
+};
+
+export default function ButtonIcon({ iconName, onPress, disabled = false }: ButtonTextProps) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      className={
+        "w-fit rounded-full bg-neutral-bg-light-200 p-2.5 active:opacity-70 dark:bg-neutral-bg-dark-200"
+      }
+      style={{
+        shadowColor: "#0A0A0A",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.15,
+        shadowRadius: 8,
+        elevation: 6, // Android
+      }}
+    >
+      <Icon family="Ionicons" name={iconName} />
+    </TouchableOpacity>
+  );
+}
