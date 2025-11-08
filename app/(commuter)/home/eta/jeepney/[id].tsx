d10ec78dd2cf8e67acfc21423fd5cd44b16af96e
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-11-08 14:03:40 +0800

    style(eta jeepney screen): Update initial eta jeepney info screen

---
+++ app/(commuter)/home/eta/jeepney/[id].tsx
@@ -0,0 +1,62 @@
+import MapLayout from "@/components/layout/MapLayout";
+import { View } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
+import { ThemedText, Icon } from "@/components/shared";
+import { JeepCard, StopCard } from "@/components/commuter";
+
+export default function EtaJeepneyInfoScreen() {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <MapLayout title="ETA Jeepney Info">
+      <View className="gap-3">
+        {/* Header Info */}
+        <View className="flex-row items-center gap-2">
+          <ThemedText variant="h600">Arriving in</ThemedText>
+          <ThemedText variant="h600" className="text-dodger-blue-600 dark:text-dodger-blue-600">
+            3 mins.
+          </ThemedText>
+        </View>
+
+        {/* Cards */}
+        <View className="gap-3">
+          {/* Jeepney Card */}
+          <View className="flex-1 gap-1">
+            <ThemedText variant="h300" className="uppercase">
+              Jeepney
+            </ThemedText>
+            <JeepCard
+              plateNo={id.toString()}
+              status="On route"
+              onPress={() => router.push("/(commuter)/home/jeepney/IAE 2730")}
+            />
+          </View>
+
+          {/* Icon Indicator */}
+          <View className="items-center">
+            <Icon family="MaterialCommunityIcons" name="arrow-down-right" size={28} />
+          </View>
+
+          {/* Stop Point Card */}
+          <View className="flex-1 gap-1">
+            <ThemedText variant="h300" className="uppercase">
+              Stop Point
+            </ThemedText>
+            <StopCard
+              location="Bingao Elementary & National High School"
+              address="Batac City"
+              onPress={() =>
+                router.push("/(commuter)/home/stop/Bingao Elementary & National High School")
+              }
+            />
+          </View>
+        </View>
+
+        {/* Footer Text */}
+        <ThemedText variant="h200" color="secondary" className="text-center">
+          You can now proceed to your nearest stop point and wait for the jeepney to arrive.
+        </ThemedText>
+      </View>
+    </MapLayout>
+  );
+}
