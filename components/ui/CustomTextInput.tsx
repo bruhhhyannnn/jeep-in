99b10c5e033cc97e2c18742e8dc23a5c17e5e00b
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-11-14 10:06:56 +0800

    edit custom text input

--- components/ui/CustomTextInput.tsx
+++ components/ui/CustomTextInput.tsx
@@ -1,38 +1,29 @@
 import React from "react";
-import { ViewProps } from "react-native";
+import { TextInputProps } from "react-native";
 import Icon from "@/components/ui/Icon";
 import ThemedView from "@/components/ui/ThemedView";
 import ThemedTextInput from "@/components/ui/ThemedTextInput";
 
-interface CustomTextInputProps extends ViewProps {
+interface CustomTextInputProps extends TextInputProps {
   iconName: string;
-  placeholder: string;
-  onChangeText?: (text: string) => void;
-  value?: string;
+  containerClassName?: string;
 }
 
 export default function CustomTextInput({
-  placeholder = "Search what?",
-  onChangeText,
-  value,
   iconName,
-  className,
-  ...props
+  placeholder = "Search what?",
+  containerClassName,
+  style,
+  ...rest
 }: CustomTextInputProps) {
   return (
     <ThemedView
       variant="bg_light"
-      className={`flex-row items-center gap-1 rounded-full px-4 ${className ?? ""}`}
-      {...props}
+      className={`flex-row items-center gap-1 rounded-full px-4 ${containerClassName ?? ""}`}
     >
       <Icon name={iconName} color="#737373" size={18} />
-      <ThemedTextInput
-        placeholder={placeholder}
-        returnKeyType="search"
-        value={value}
-        onChangeText={onChangeText}
-        style={{ flex: 1 }}
-      />
+
+      <ThemedTextInput placeholder={placeholder} style={[{ flex: 1 }, style]} {...rest} />
     </ThemedView>
   );
 }
